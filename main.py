85de121be09e61cb2bd930c579a91bf7bf6cb12b
AWAIKEN1 <alistairaiken@gmail.com>
2023-09-15 15:57:35 +0100

    Working through Day 12- 60 Python Coding Challenge

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ def get_todos(filepath):
         todos_local = file_local.readlines()
     return todos_local
 
+def write_todos(filepath, todos_arg):
+    with open(filepath, 'w') as file:
+        file.writelines(todos_arg)
+
+
 while True:
     # Get user input and strip space chars from it
     user_action = input("Type add, show, edit, complete or exit: ")
@@ -15,8 +20,8 @@ while True:
 
         todos.append(todo + '\n')
 
-        with open('todos.txt', 'w') as file:
-            file.writelines(todos)
+        write_todos("todos.txt", todos)
+
 
     elif user_action.startswith('show'):
 
@@ -39,8 +44,7 @@ while True:
             new_todo = input("Enter new todo: ")
             todos[number] = new_todo + '\n' #need \n to create new row in list
 
-            with open('todos.txt', 'w') as file:
-                file.writelines(todos)
+            write_todos("todos.txt", todos)
         except ValueError:
             print("Your command is not valid.")
             continue
@@ -54,8 +58,7 @@ while True:
             todo_to_remove = todos[index].strip('\n')
             todos.pop(index)
 
-            with open('todos.txt', 'w') as file:
-                file.writelines(todos)
+            write_todos("todos.txt", todos)
 
             message = f"Todo {todo_to_remove} was removed from the list."
             print(message)
