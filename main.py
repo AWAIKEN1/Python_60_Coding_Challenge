3ea41f5c3c8bbded4eb24c3d1ec73b24194d4b9a
AWAIKEN1 <alistairaiken@gmail.com>
2024-01-03 17:02:09 +0000

    Continued with Day 15-Adding the date and time to my app. Learning about standard modules

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 # from functions import get_todos, write_todos
 import functions
+import time
 
+now = time.strftime("%b %d, %Y %H:%M:%S")
+print(f"It is {now}")
 while True:
     # Get user input and strip space characters from it
     user_action = input("Type add, show, edit, complete or exit: ")
